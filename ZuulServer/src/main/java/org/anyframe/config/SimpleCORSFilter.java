66a286916050bb4763be98f21ce46c934df9c4fc
Seulki Hahn <sds.han@samsung.com>
2015-09-06 18:25:31 +0900

    CORS filter add PUT method

--- ZuulServer/src/main/java/org/anyframe/config/SimpleCORSFilter.java
+++ ZuulServer/src/main/java/org/anyframe/config/SimpleCORSFilter.java
@@ -16,9 +16,9 @@ public class SimpleCORSFilter implements Filter {
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
 		chain.doFilter(req, res);
 	}
 
